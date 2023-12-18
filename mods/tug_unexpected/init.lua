d70d6e547134f166e9a418ba2290ad0e76d5f7c7
Flederossi <oskar.web.thieme@gmail.com>
2023-12-18 22:28:34 +0100

    Added placeholders for behaviors

--- mods/tug_unexpected/init.lua
+++ mods/tug_unexpected/init.lua
@@ -2,19 +2,75 @@ tug_unexpected = {
     unexpected_behaviors = {
         {
             name = "Remove all pawns",
-            pick_min = 1,
-            pick_max = 70,
+            pick_min = 0,
+            pick_max = 0,
             func = function()
                 -- TODO: Implement this thing
             end
         },
         {
             name = "Pawns storm",
-            pick_min = 71,
-            pick_max = 100,
+            pick_min = 0,
+            pick_max = 0,
             func = function()
                 -- TODO: Implement this thing
             end
         },
+		{
+			name = "Swap player",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Delete piece",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Add piece",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Swap queens",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Swap two pieces",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Showdown",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
+		{
+			name = "Time travel",
+			pick_min = 0,
+			pick_max = 0,
+			func = function()
+                -- TODO: Implement this thing
+            end
+		},
     }
 }
