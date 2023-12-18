a2ecc25a9730946b17dd3f7c884fc4001f236711
NO411 <weiler.noah2006@gmail.com>
2023-12-18 22:17:19 +0100

    fix crash

--- mods/tug_unexpected/init.lua
+++ mods/tug_unexpected/init.lua
@@ -1,18 +1,20 @@
-tug_unexpected.unexpected_behaviors = {
-	{
-		name = "Remove all pawns",
-		pick_min = 1,
-		pick_max = 70,
-		func = function()
-			-- TODO: Implement this thing
-		end
-	},
-	{
-		name = "Pawns storm",
-		pick_min = 71,
-		pick_max = 100,
-		func = function()
-			-- TODO: Implement this thing
-		end
-	},
+tug_unexpected = {
+    unexpected_behaviors = {
+        {
+            name = "Remove all pawns",
+            pick_min = 1,
+            pick_max = 70,
+            func = function()
+                -- TODO: Implement this thing
+            end
+        },
+        {
+            name = "Pawns storm",
+            pick_min = 71,
+            pick_max = 100,
+            func = function()
+                -- TODO: Implement this thing
+            end
+        },
+    }
 }
