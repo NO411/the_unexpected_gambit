3dd66e4494bb0a95b4a33f9ca2a2993cadd4c8a3
Flederossi <oskar.web.thieme@gmail.com>
2023-12-19 16:37:10 +0100

    Implemented some behaviors

--- mods/tug_unexpected/init.lua
+++ mods/tug_unexpected/init.lua
@@ -5,8 +5,14 @@ tug_unexpected = {
             pick_min = 0,
             pick_max = 0,
             func = function()
-                -- TODO: Implement this thing
-            end
+				for l, line in pairs(tug_gamestate.g.current_board) do
+					for r, row in pairs(line) do
+						if string.lower(row.name) == "p" then
+							tug_gamestate.g.current_board[l][r] = {name = ""}
+						end
+					end
+				end	
+			end
         },
         {
             name = "Pawns storm",
@@ -45,7 +51,20 @@ tug_unexpected = {
 			pick_min = 0,
 			pick_max = 0,
 			func = function()
-                -- TODO: Implement this thing
+				local queens = {}
+				for l, line in pairs(tug_gamestate.g.current_board) do
+					for r, row in pairs(line) do
+						if string.lower(row.name) == "q" then
+							queens[row.name] = {l=l, r=r}
+						end
+					end
+				end
+				local queen_num = 0
+				for k, v in pairs(queens) do queen_num = queen_num + 1 end
+				if queen_num == 2 then
+					tug_gamestate.g.current_board[queens["q"].l][queens["q"].r] = {name = "Q"}
+					tug_gamestate.g.current_board[queens["Q"].l][queens["Q"].r] = {name = "q"}
+				end
             end
 		},
 		{
@@ -53,16 +72,22 @@ tug_unexpected = {
 			pick_min = 0,
 			pick_max = 0,
 			func = function()
-                -- TODO: Implement this thing
-            end
+				-- TODO: Implement this thing
+			end
 		},
 		{
 			name = "Showdown",
 			pick_min = 0,
 			pick_max = 0,
 			func = function()
-                -- TODO: Implement this thing
-            end
+				for l, line in pairs(tug_gamestate.g.current_board) do
+					for r, row in pairs(line) do
+						if string.lower(row.name) ~= "k" and string.lower(row.name) ~= "q" then
+							tug_gamestate.g.current_board[l][r] = {name = ""}
+						end
+					end
+				end
+			end
 		},
 		{
 			name = "Time travel",
